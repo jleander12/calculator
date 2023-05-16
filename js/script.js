054175d02d888b05880509f1f17e914c5a5dd2a6
Jared Leander <jleander12@gmail.com>
2023-05-16 12:17:29 -0700

    Add basic buttons and event listener to get char
    Buttons for all of the numbers and operations were added, as well as
    event listeners to catch clicks on them. themhe event listener calls the
    new getChar function which records the innerHTML property.
    
    Next steps:
     - Use get char to identify if the input is a value, operation,
    or submit
     - Convert the strings into numbers and call the operation
    function
     - Format and style the calculator
     - Add button click animation for user feedback
    
     Should there be keyboard support in addition to on-screen

--- js/script.js
+++ js/script.js
@@ -3,17 +3,6 @@ let secondNum = 0;
 let calcOperation = ''; //+ for add, - for subtract, * for multiply, / for divide
 let solution = 0;
 
-operate(2,5,'+');
-operate(2,5,'-');
-operate(2,5,'*');
-operate(2,5,'/');
-operate(2,5,'j');
-operate(3,5,'+');
-operate(3,5,'-');
-operate(3,5,'*');
-operate(3,5,'/');
-operate(3,5,'j');
-
 function operate(a, b, operation) {
     if (operation === '+') {
         solution = add(a, b);
@@ -44,3 +33,11 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function getChar(e) {
+    console.log(this.innerHTML);
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', getChar));
+
